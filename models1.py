0e10e6412c2d904afe2c7e5ab7f841143dc1c979
CerJesus <jecervantes14@gmail.com>
2018-11-18 16:29:51 -0800

    Update models1.py

--- models1.py
+++ models1.py
@@ -8,15 +8,15 @@ import numpy as np
 def get_features_labels(file):
 
     training_data = pd.read_csv(file, index_col = [0])
-    training_data = pd.DataFrame(training_data, columns = ["time", "temperature", "pressure", "press_change", "label"])
+    training_data = pd.DataFrame(training_data, columns = ["time", "temperature", "pressure", "press_change", "var", "label"])
 
     #the last three pressure readings
-    training_data["press_1"] = training_data["pressure"].shift(1)
-    training_data["press_2"] = training_data["pressure"].shift(2)
-    training_data["press_3"] = training_data["pressure"].shift(3)
+    training_data["press_change_1"] = training_data["press_change"].shift(1)
+    training_data["press_change_2"] = training_data["press_change"].shift(2)
+    training_data["press_change_3"] = training_data["press_change"].shift(3)
 
     training_data = training_data.dropna(axis=0)
-    x = training_data[["temperature", "pressure", "press_change", "press_1", "press_2", "press_3"]]
+    x = training_data[["temperature", "pressure", "press_change", "press_change_1", "press_change_2", "press_change_3", "var"]]
     y = training_data["label"]
     return(x,y)
 
@@ -29,6 +29,9 @@ def test_predict(model, features):
 x,y=get_features_labels("ssi_pressure_labels.csv")
 
 k = 10
+
+
+clf = SVC()
 
 #Logistic regression
 lr = LogisticRegression()
@@ -64,7 +67,22 @@ print(test_x.columns)
 print("SVM score on test data (pressure-based labels): ", clf.score(test_x, test_y))
 test_predict(clf,test_x)
 
+
+
 #Random Forest
 rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
 rfscores = cross_val_score(rf, x, y, cv = k)
 print("Random Forest Cross Validation Scores for k = ", k, ": ", rfscores)
+rf.fit(x,y)
+
+#Predictions on test set (labeled based on altitude)
+test_x,test_y = get_features_labels("ssi_pressure_test1_badaltitude.csv")
+print(test_x.columns)
+print("Random Forest score on test data (altitude-based labels): ", rf.score(test_x, test_y))
+test_predict(rf,test_x)
+
+#Predictions on test set (labeled based on pressure)
+test_x,test_y = get_features_labels("ssi_pressure_test1_badpressure.csv")
+print(test_x.columns)
+print("Random Forest score on test data (pressure-based labels): ", rf.score(test_x, test_y))
+test_predict(rf,test_x)
